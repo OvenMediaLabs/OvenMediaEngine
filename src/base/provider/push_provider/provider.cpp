b64df8c7a95b42e4aa2866e845e0e8af217a292f
Hyunjun Jang <dimiden@ovenmedia.com>
2026-07-31 00:58:31 +0900

    refactor: change log level

--- src/base/provider/push_provider/provider.cpp
+++ src/base/provider/push_provider/provider.cpp
@@ -264,7 +264,7 @@ namespace pvd
 				const auto elapsed_ms = static_cast<intmax_t>(state.elapsed_ms);
 				const auto timeout_ms = static_cast<intmax_t>(state.timeout_ms);
 
-				logtt("Checking channel %u, elapsed %" PRIdMAX " ms, timeout %" PRIdMAX " ms", channel->GetChannelId(),
+				logtd("Checking channel %u, elapsed %" PRIdMAX " ms, timeout %" PRIdMAX " ms", channel->GetChannelId(),
 					  elapsed_ms,
 					  timeout_ms);
 
